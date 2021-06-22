6e30b8e0fdd66c4de1ccd6dcad05d72342a9f641
Marto929 <72599446+Marto929@users.noreply.github.com>
2021-06-22 22:33:58 +0300

    Add files via upload

---
+++ TicTacToeControll.java
@@ -0,0 +1,19 @@
+package domashnoit;
+
+
+	import java.awt.BorderLayout;
+
+	import javax.swing.JLabel;
+	import javax.swing.JPanel;
+	import javax.swing.SwingConstants;
+
+	public class TicTacToeControll extends JPanel{
+		private TicTacToePanel ticTacToePanel = new TicTacToePanel();
+		public static JLabel textLbl = new JLabel("Player x's turn", SwingConstants.CENTER);
+		
+		public TicTacToeControll() {
+			setLayout(new BorderLayout());
+			add(ticTacToePanel, BorderLayout.CENTER);
+			add(textLbl, BorderLayout.NORTH);
+		}
+}
